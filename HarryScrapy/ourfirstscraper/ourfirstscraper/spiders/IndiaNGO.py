f6d66019365458f708698e3bc6e855ed56b3dada
Hsong159 <77717780+Hsong159@users.noreply.github.com>
2021-11-15 12:45:51 -0800

    done with adding comments

--- HarryScrapy/ourfirstscraper/ourfirstscraper/spiders/IndiaNGO.py
+++ HarryScrapy/ourfirstscraper/ourfirstscraper/spiders/IndiaNGO.py
@@ -8,23 +8,29 @@ class IndiangoSpider(scrapy.Spider):
     start_urls = ['https://www.indiangoslist.com/']
 
     def parse(self,response):
+        #get all hyperlinks of states on the home page
         ngo_links = response.css("li > h3 > a::attr('href')")
-        print(ngo_links)
+        #for each links, call function parse_one
         yield from response.follow_all(ngo_links,self.parse_one) 
     def parse_one(self, response):
+        #For each NGO on that page, get it into ngo_scrape
         ngo_scrape = response.css(".title > a::attr('href')")
+        #call parse_two on every ngo from ngo_scrape
         yield from response.follow_all(ngo_scrape,self.parse_two)
-         
+        #gets all links from the pagination, note that scrapy automatically make sure they don't visit same links twice.
         ngo_next = response.css(".pagination > a::attr('href')")
-        #make sure ngo_next is unique
+        #call parse_one on every links because each of these links contain multiple NGO
         yield from response.follow_all(ngo_next,self.parse_one) 
 
     def parse_two(self, response):
+        #get the left row
         ngo_left = response.css(".ngo_left_head::text").extract()
         ngo_right = response.css(".ngo_right_head::text").extract()
+        #get the right row
         span = response.xpath("//*[@class='ngo_right_head']//text()").extract()
         count_1 = 0
         count_2 = 0
+        #remove space and "/n"
         for i in range(len(span)):
             if span[i] == ' ':
                 count_1 += 1
@@ -34,9 +40,12 @@ class IndiangoSpider(scrapy.Spider):
             span.remove(' ')
         for _ in range(count_2):
             span.remove('\n')
+        #remove space if exist
         if ' ' in ngo_left:
             ngo_left.remove(' ')
+        #make sure the left row and right row oare the same length after fixing key issues
         span = span[0:len(ngo_left)+1]
+        #since the key issue has two part, we have to indentify them and add them together
         if ' Key Issues ' in ngo_left:
             if ngo_left[len(ngo_left)-2] == ' Operational States ' and ngo_left[len(ngo_left)-1] == ' Operational Districts ':
                 span[len(span)-4] = span[len(span)-4] + span[len(span)-3]
@@ -50,6 +59,7 @@ class IndiangoSpider(scrapy.Spider):
             else:
                 span[len(span)-2] = span[len(span)-2] + span[len(span)-1]
                 span = span[0:len(span)-1]
+        #export the result
         for item in zip(ngo_left,span):
 
             scraped = {
