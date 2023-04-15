4f8822b0c70128ce9c5238a0cb026daaa10a3539
zhouyijun111 <yijunzhou@ocf.berkeley.edu>
2023-04-14 23:25:22 -0700

    updated

--- ngodarpan_scraper/ngodarpan_scraper/items.py
+++ ngodarpan_scraper/ngodarpan_scraper/items.py
@@ -4,9 +4,39 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+from datetime import datetime
 
+def remove_newlines(s):
+    return s.replace('\n|\r|\t', '')
+
+def replace_newlines(s):
+    return s.replace('\n|\r|\t', ' ')
 
 class NgodarpanScraperItem(scrapy.Item):
-    # define the fields for your item here like:
+    # define all the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    SrNo = scrapy.Field()
+
+    Name = scrapy.Field(
+        input_processor=MapCompose(str.strip, replace_newlines, str.title),
+        output_processor=TakeFirst()
+    )
+
+    Registration = scrapy.Field(
+        input_processor=MapCompose(str.strip, replace_newlines, str.title),
+        output_processor=TakeFirst()
+    )
+
+    Address = scrapy.Field(
+        input_processor=MapCompose(str.strip, replace_newlines, str.title),
+        output_processor=TakeFirst()
+    )
+
+    SectorWorking = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    
+    
